73271f5df034afde1dd680423087accdc16b80d5
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-09-10 20:55:46 +0300

    Basic offline page

--- app/components/offlineHandler/offlineHandler.tsx
+++ app/components/offlineHandler/offlineHandler.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 
 const OfflineHandler = ({ children }: { children: React.ReactNode }) => {
     React.useEffect(() => {
-        
+        console.log('OfflineHandler mounted');
+        if (!navigator) return;
+        if ("serviceWorker" in navigator) {
+            const swUrl = "/offlineWorker.js";
+            navigator.serviceWorker
+                .register(swUrl)
+                .then((registration) => {
+                    console.log("ServiceWorker registration successful with scope: ", registration.scope);
+                })
+                .catch((error) => {
+                    console.error("ServiceWorker registration failed: ", error);
+                });
+        }
     }, []);
 
     return <>{ children }</>;
---
+++ public/offlineWorker.js
@@ -0,0 +1,49 @@
+// service-worker.js
+
+const cacheName = 'offlineCacheVersion1';
+
+const offlinePages = [
+    '/'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches
+            .open(cacheName)
+            .then((cache) => {
+                // Using cacheName for opening cache
+                return cache.addAll(offlinePages); // Cache multiple pages
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        // Checking if cache name is not 'v1'
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch((error) => {
+            return caches.match(e.request).then((response) => {
+                if (response) {
+                    return response;
+                } else if (e.request.headers.get('accept').includes('text/html')) {
+                    // If the request is for an HTML page, return the offline page
+                    return caches.match('/offline.html');
+                }
+            });
+        })
+    );
+});
