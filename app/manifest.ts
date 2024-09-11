288d4add355037438d745613eb76394c287a2327
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-09-11 22:30:51 +0300

    Updated the webmanifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,14 +8,22 @@ export default function manifest(): MetadataRoute.Manifest {
     id: '/',
     start_url: '/',
     display: 'standalone',
+    orientation: 'portrait',
+    scope: '/',
     background_color: '#454937',
     theme_color: '#454937',
+    file_handlers: [],
     icons: [
       {
         src: '/favicon.png',
         sizes: '512x512',
         type: 'image/png',
       },
+      {
+        src: '/favicon.ico',
+        sizes: '256x256 128x128',
+        type: 'image/x-icon'
+      }
     ],
   }
 }
