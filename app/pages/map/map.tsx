ed04c09429f3dca8e3cba543e85eb319e213b20b
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-10-08 14:00:58 +0300

    removed the zoom control from the map

--- app/pages/map/map.tsx
+++ app/pages/map/map.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import L, { LatLngExpression, } from 'leaflet';
-import InfoCard, { cardType } from "@app/components/infocard";
-import React from "react";
-import { location } from "@app/logic/types";
-import "leaflet/dist/leaflet.css";
+import InfoCard, { cardType } from '@app/components/infocard';
+import React from 'react';
+import { location } from '@app/logic/types';
+import 'leaflet/dist/leaflet.css';
 
 const playerPin = new L.Icon({
     iconUrl: '/icons/userpin.svg',
@@ -16,6 +16,17 @@ const poiPin = new L.Icon({
     iconUrl: '/icons/poipin.svg',
     iconSize: [ 26, 37 ],
 });
+
+const HookComponent = () => {
+    const map: L.Map = useMap();
+
+    React.useEffect(() => {
+        if (map)
+            map.invalidateSize();
+    });
+
+    return null;
+}
 
 const Map = (
     props: {
@@ -29,7 +40,7 @@ const Map = (
     const [ name,    setName    ] = React.useState<string | undefined>(undefined);
     const center: LatLngExpression = props.userLocation
         ? [ props.userLocation.lat, props.userLocation.lng ]
-        : [ 42.143013705260884, 24.749279022216797 ];
+        : [ 42.143013705260884, 24.749279022216797 ]; // Center of Plovdiv
 
     return (
         <>
@@ -43,14 +54,16 @@ const Map = (
                 />
             }
             <MapContainer
+                zoomControl={ false }
                 center={ center }
                 zoom={ 19 }
                 scrollWheelZoom={ true }
-                style={ { height: "100%", width: "100%" } }
+                style={ { height: '100%', width: '100%' } }
             >
+                <HookComponent />
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
                 {
                     props.userLocation &&
