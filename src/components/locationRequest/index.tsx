cb850f4d9faaee76e64b1211636085c78aba3696
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-11-08 10:18:32 +0200

    request geolocation access on accepting the prompt

--- src/components/locationRequest/index.tsx
+++ src/components/locationRequest/index.tsx
@@ -9,15 +9,24 @@ type RequestProps = {
 const LocationRequest: React.FC<RequestProps> = ({ setPermission }) => {
     const reference = React.useRef<HTMLDivElement>(null);
 
-    const close = (hasGPSAccess: boolean) => {
+    const close = () => {
         if(!reference.current) return;
         const parent = reference.current.parentElement;
         if(!parent) return;
     
         reference.current.style.animation = `${ style.slideOut } 0.5s ease-in-out forwards`;
         parent.style.animation            = `${ style.blurOut  } 0.5s ease-in-out forwards`;
-    
-        setTimeout(() => setPermission(hasGPSAccess), 500);
+    }
+
+    const accept = () =>
+        navigator.geolocation.getCurrentPosition(() => {
+            close();
+            setTimeout(() => setPermission(true), 500);
+        });
+
+    const decline = () => {
+        close();
+        setTimeout(() => setPermission(false), 500);
     }
 
     return (
@@ -25,8 +34,8 @@ const LocationRequest: React.FC<RequestProps> = ({ setPermission }) => {
             <div ref={ reference }>
                 <h3>Location</h3>
                 <p>You will need to allow geolocation to claim badges.</p>
-                <button onClick={ () => close(true)  }>Allow</button>
-                <button onClick={ () => close(false) }>Deny</button>
+                <button onClick={ accept  }>Allow</button>
+                <button onClick={ decline }>Deny</button>
             </div>
         </div>
     );
