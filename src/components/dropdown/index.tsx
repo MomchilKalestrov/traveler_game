1788a46a6ee7ba74ce5d4dbd99f00ac2ea84b82f
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-12-01 19:55:23 +0200

    implement the drop down

---
+++ src/components/dropdown/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Image from 'next/image';
+
+import arrow from './arrow.svg';
+
+import style from './dropdown.module.css';
+
+type DropdownProps = {
+    entries: { [key: string]: string };
+    name: string;
+    selected?: string;
+    width?: string;
+    onClick?: (event: React.MouseEvent<HTMLDivElement>, value: string) => void;
+};
+
+const Dropdown: React.FC<DropdownProps> = ({ entries, name, selected, width, onClick }) => {
+    console.log(selected);
+    return(
+    <div className={ style.Dropdown } style={ { width: width } }>
+        <div className={ style.DropdownHead }>
+            <p>{ name }</p>
+            <input type='checkbox' />
+            <Image src={ arrow } alt='arrow' width={ 24 } height={ 24 } />
+        </div>
+        <div
+            className={ style.DropdownBody }
+            style={ {
+                '--dropdown-height': `calc(1px + 3rem * ${ Object.entries(entries).length })`,
+                width: width
+            } as React.CSSProperties }
+        >
+        <div className={ style.DropdownDivider } />
+            {
+                Object.entries(entries).map(([key, value]) => (
+                    <label key={ key } className={ style.DropdownLabel }>
+                        <input
+                            type='radio'
+                            name={ name }
+                            value={ value }
+                            defaultChecked={ value === selected }
+                            onClick={ (e) => onClick ? onClick(e, value) : null }
+                        />
+                        <p>{ key }</p>
+                    </label>
+                ))
+            }
+        </div>
+    </div>
+)};
+
+export default Dropdown;
