2436a0ea4aa2ba8ef87d27cd84b9e57107c082f8
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-12-09 13:26:35 +0200

    Remove console.log()

--- src/app/[lang]/map/page.tsx
+++ src/app/[lang]/map/page.tsx
@@ -13,67 +13,62 @@ import LoadingPlaceholder from '@components/loading';
 import Dialog from '@components/dialog';
 
 const Page: NextPage = () => {
-  const language: Language | undefined = React.useContext(LanguageCTX);
+    const language: Language | undefined = React.useContext(LanguageCTX);
 
-  const started  = useSelector((state: RootState) => state.started.value);
+    const started  = useSelector((state: RootState) => state.started.value);
 
-  const [ allowGPS, setPermission ] = React.useState<boolean>(true);
-  const [ request,  setRequest    ] = React.useState<boolean>(false);
+    const [ allowGPS, setPermission ] = React.useState<boolean>(true);
+    const [ request,  setRequest    ] = React.useState<boolean>(false);
 
-  const Map = React.useMemo(() => dynamic(
-    () => import('./map'), {
-      loading: () => (<LoadingPlaceholder />),
-      ssr: false
-    }
-  ), [])
+    const Map = React.useMemo(() => dynamic(
+        () => import('./map'), {
+            loading: () => (<LoadingPlaceholder />),
+            ssr: false
+        }
+    ), [])
 
-  React.useEffect(() => {
-    preloadFromSessionStorage();
-  }, []);
+    React.useEffect(() => {
+        preloadFromSessionStorage();
+    }, []);
 
-  React.useEffect(() => {
-    navigator.permissions.query({ name: 'geolocation' })
-      .then((permission) =>
-        setRequest(permission.state !== 'granted')
-      );
-  }, []);
+    React.useEffect(() => {
+        navigator.permissions.query({ name: 'geolocation' })
+            .then((permission) =>
+                setRequest(permission.state !== 'granted')
+            );
+    }, []);
 
-  const setRequestPermission = (hasGPSAccess: boolean): void => {
-    setPermission(hasGPSAccess);
-    console.log(hasGPSAccess);
-  };
+    const setRequestPermission = (hasGPSAccess: boolean): void =>
+        setPermission(hasGPSAccess);
 
-  if (!language) return (<LoadingPlaceholder />);
+    if (!language) return (<LoadingPlaceholder />);
 
-  return (
-    <main style={ { padding: '0px', height: 'calc(100vh - 5rem)' } }>
-      {
-        request &&
-        <Dialog
-          title={ language.misc.GPSaccess.title }
-          description={ language.misc.GPSaccess.description }
-          options={ [
-            {
-              name: language.misc.GPSaccess.decline,
-              event: () => setRequestPermission(false)
-            },
-            {
-              name: language.misc.GPSaccess.accept,
-              event: () => {
-                navigator.geolocation.getCurrentPosition(
-                  () => setRequestPermission(true),
-                  () => setRequestPermission(false)
-                );
-              },
-              primary: true
-            }
-          ] }
-          close={ setRequest }
-        />
-      }
-      <Map locations={ started } hasGPSAccess={ allowGPS } />
-    </main>
-  );
+    return (
+        <main style={ { padding: '0px', height: 'calc(100vh - 5rem)' } }>
+        {
+            request &&
+            <Dialog
+                title={ language.misc.GPSaccess.title }
+                description={ language.misc.GPSaccess.description }
+                options={ [ {
+                    name: language.misc.GPSaccess.decline,
+                    event: () => setRequestPermission(false)
+                }, {
+                    name: language.misc.GPSaccess.accept,
+                    event: () => {
+                        navigator.geolocation.getCurrentPosition(
+                            () => setRequestPermission(true),
+                            () => setRequestPermission(false)
+                        );
+                    },
+                    primary: true
+                } ] }
+                close={ setRequest }
+            />
+        }
+            <Map locations={ started } hasGPSAccess={ allowGPS } />
+        </main>
+    );
 };
 
 export default Page;
