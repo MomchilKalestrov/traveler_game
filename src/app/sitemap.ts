1121dd21f4e361ca8e52060c52e7a21ea9c5d9e9
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-12-14 12:12:07 +0200

    refactor sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,37 +2,17 @@ import type { MetadataRoute } from 'next';
 
 const baseURL = 'https://traveler-game.vercel.app';
 
+const pages: string[] = [ 'login', 'about', 'home', 'map', 'profile', 'leaderboard', 'about', 'download' ];
+const languages: string[] = [ 'bg', 'en' ];
+
 const sitemap = (): MetadataRoute.Sitemap => ([
-    {
-        url: baseURL + '/',
-        lastModified: '01-11-2024',
-        changeFrequency: 'never'
-    },
-    {
-        url: baseURL + '/leaderboard',
-        lastModified: '01-11-2024',
-        changeFrequency: 'monthly'
-    },
-    {
-        url: baseURL + '/profile',
-        lastModified: '01-11-2024',
-        changeFrequency: 'weekly'
-    },
-    {
-        url: baseURL + '/map',
-        lastModified: '01-11-2024',
-        changeFrequency: 'weekly'
-    },
-    {
-        url: baseURL + '/home',
-        lastModified: '01-11-2024',
-        changeFrequency: 'weekly'
-    },
-    {
-        url: baseURL + '/login',
-        lastModified: '01-11-2024',
-        changeFrequency: 'monthly'
-    }
+    ... languages.flatMap(language =>
+            pages.map(page => ({
+                url: `${ baseURL }/${ language }/${ page }`,
+                lastModified: '14-12-2024',
+                changeFrequency: 'weekly' as 'weekly',
+            }))
+        )
 ]);
 
 export default sitemap;
