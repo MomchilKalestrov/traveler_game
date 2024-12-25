c4ca05460b86cb944e8eb9af40ac5555de2b75f1
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-12-25 20:28:08 +0200

    Extract the homepage segments to different components

--- src/app/[lang]/home/page.tsx
+++ src/app/[lang]/home/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import Image from 'next/image';
 import { NextPage }    from 'next';
+import { useSelector } from 'react-redux';
 
 import Button   from '@components/button';
 import Mapcard  from '@components/mapcard';
@@ -14,13 +15,68 @@ import LanguageCTX   from '@logic/contexts/languageCTX';
 import {
     Location,
     Accomplishment,
-    User,
     Language
 } from '@logic/types';
 
 import { RootState } from '@logic/redux/store';
 
 import style from './home.module.css';
+
+type NewLocationsProps = {
+    newLocations: Location[] | undefined;
+    language: Language;
+    maxNewLocations: number;
+};
+
+type StartedLocationsProps = {
+    startedLocations: Location[] | undefined;
+    language: Language;
+};
+
+const NewLocations: React.FC<NewLocationsProps> = ({
+    newLocations,
+    language,
+    maxNewLocations
+}) => {
+    if (!newLocations) return (
+        <div className={ style.LoadingSegment }>
+            <Image src='/icons/loading.svg' width={ 64 } height={ 64 } alt='loading' />
+        </div>
+    );
+
+    if (newLocations.length === 0) return (<p>{ language.home.alts.new }</p>);
+
+    // synthetically cap the maximum so that the home page doesn't look too cluttered
+    // this is temporary until I implement different sections like
+    // "mountains", "beaches", "cities" and so on
+    
+    return newLocations.slice(0, maxNewLocations).map((location: Location, index: number) =>
+        <Mapcard key={ index } location={ location } />
+    );
+};
+
+const StartedLocations: React.FC<StartedLocationsProps> = ({
+    startedLocations,
+    language
+}) => {
+    if (!startedLocations) return (
+        <div className={ style.LoadingSegment }>
+            <Image src='/icons/loading.svg' width={ 44 } height={ 44 } alt='loading' />
+        </div>
+    );
+
+    if (startedLocations.length === 0) return (<p>{ language.home.alts.started }</p>);
+
+    return (
+        <div className={ style.HorizontalCarousel }>
+            <div>
+                { startedLocations.map((location: Location, key: number) =>
+                    <Minicard key={ key } location={ location } />
+                ) }
+            </div>
+        </div>
+    );
+}
 
 const Page: NextPage = () => {
     const language: Language | undefined = React.useContext(LanguageCTX);
@@ -40,57 +96,35 @@ const Page: NextPage = () => {
     ] = React.useState<number>(Number(process.env.NEXT_PUBLIC_MAX_LOCATIONS || '6'));
 
     React.useEffect(() => {
-        if (!userSlice) return;
-        getActivities(userSlice as User, allSlice as Location[]).then(setFollowerActivity);
-    }, [userSlice]);
+        if (!userSlice || !allSlice) return;
+        getActivities(userSlice, allSlice).then(setFollowerActivity);
+    }, [ userSlice ]);
 
-    if (!userSlice || !newSlice || !startedSlice || !language) return (<LoadingPlaceholder />);
-
-    console.log(newSlice)
+    if (!language) return (<LoadingPlaceholder />);
 
     return (
         <main className={ style.Home }>
             <h2>{ language.home.titles.started }</h2>
-        { 
-            !startedSlice || startedSlice.length === 0
-            ?   <p>{ language.home.alts.started }</p>
-            :   <div className={ style.HorizontalCarousel }><div>
-            {
-                startedSlice.map((location: Location, key: number) =>
-                <Minicard key={ key } location={ location } />
-                )
-            }
-            </div></div>
-        }
+            <StartedLocations startedLocations={ startedSlice } language={ language } />
+
             <h2>{ language.home.titles.new }</h2>
-        { [
-            (
-                newSlice.length === 0
-                ?   <p>{ language.home.alts.new }</p>
-                // synthetically cap the maximum so that the home page doesn't look too cluttered
-                // this is temporary until I implement different sections like
-                // "mountains", "beaches", "cities" and so on
-                :   (newSlice.slice(0, maxNewLocations)).map((
-                        location: Location,
-                        index: number
-                    ) => <Mapcard key={ index } location={ location } />)
-            ),
-            (
-                newSlice.length > maxNewLocations
-                ?   <Button
-                        border={ true }
-                        onClick={ () => setMaxNewLocations(maxNewLocations + 6) }
-                    >{ language.home.loadMore }</Button>
-                :   null
-            )
-        ] }
+            <NewLocations newLocations={ newSlice } maxNewLocations={ maxNewLocations } language={ language } />
         {
-            followerActivity.length > 0 && [
-                <h2>{ language.home.titles.activity }</h2>,
-                followerActivity.map((accomplishment: Accomplishment, key: number) => 
-                    <AccomplishmentTag key={ key } accomplishment={ accomplishment } />
-                )
-            ]
+            (newSlice && newSlice.length > maxNewLocations) &&
+            <Button
+                border={ true }
+                onClick={ () => setMaxNewLocations(maxNewLocations + 6) }
+            >{ language.home.loadMore }</Button>
+        }
+        {
+            followerActivity.length > 0 && (
+                <>
+                    <h2>{ language.home.titles.activity }</h2>
+                    { followerActivity.map((
+                        accomplishment: Accomplishment, key: number
+                    ) => <AccomplishmentTag key={ key } accomplishment={ accomplishment } />) }
+                </>
+            )
         }
         </main>
     );
