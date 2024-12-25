0e5a0768351115b9bb2aeb6f36ee89bbd774db52
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-12-25 18:32:58 +0200

    Deprecate the `/api/finished` and `/api/started` routes to avoid unnecessary duplication of data

--- src/logic/redux/sessionStorage.ts
+++ src/logic/redux/sessionStorage.ts
@@ -1,16 +1,26 @@
 'use client';
-import {
-    fetchAll,
-    fetchFinished,
-    fetchProfile,
-    fetchStarted,
-    filterNew
-} from '@logic/fetches';
 import { toLocation }     from '@logic/types';
 import { Location, User } from '@logic/types';
 import store              from './store';
 
-type fetchType = [ Location[], Location[], Location[], User ];
+type fetchType = [ Location[], Location[], User ];
+
+const fetchAll = async (): Promise<any> =>
+    await (await fetch(`/api/locations`)).json();
+
+const fetchProfile = async (): Promise<User | boolean> =>
+    (await fetch(`/api/auth/get?username=CURRENT_USER`))
+        .json()
+        .then(data => data.error ? false : data)
+
+const getStarted = (all: Location[], user: User): Location[] =>
+    all.filter((l: Location) => user.started.includes(l.dbname));
+
+const getNew = (all: Location[], user: User): Location[] =>
+    all.filter((l: Location) =>
+        !user.started.includes(l.dbname) &&
+        !user.finished.find(f => f.location === l.dbname)
+    );
 
 const save = (key: string, value: any) =>
     sessionStorage.setItem(key, JSON.stringify(value));
@@ -22,18 +32,20 @@ const cast = (object: any): Location[] =>
     Array.isArray(object) ? object.map(toLocation) : [];
 
 const initialSave = async (): Promise<fetchType> => {
-    const started  = cast(await fetchStarted());
-    const finished = cast(await fetchFinished());
+    const user = await fetchProfile();
+
+    if (!user)
+        throw new Error('Error fetching user data.');
+
     const all      = cast(await fetchAll());
-    const user     = await fetchProfile();
+    const started  = getStarted(all, user as User);
 
     save('started',     started);
-    save('finished',    finished);
     save('all',         all);
     save('user',        user);
     save('initialSave', true);
     
-    return [ started, finished, all, (user ? user : {}) as User ];
+    return [ started, all, (user ? user : {}) as User ];
 }
 
 const saveToSessionStorage = (state: any) => {
@@ -46,21 +58,20 @@ const getFromSessionStorage = async (): Promise<fetchType> => {
         return await initialSave();
     else return [
         get('started'),
-        get('finished'),
         get('all'),
         get('user')
     ];
 };
 
 const preloadFromSessionStorage = async (): Promise<void> => {
-    const [ started, finished, all, user ] = await getFromSessionStorage();
+    const [ started, all, user ] = await getFromSessionStorage();
     store.dispatch({
         type: 'started/update',
         payload: started
     });
     store.dispatch({
         type: 'new/update',
-        payload: filterNew(started, finished, all)
+        payload: getNew(all, user)
     });
     store.dispatch({
         type: 'user/update',
